d31cec95088910063afeba91af398524a9906e50
Kinght <73056047+2sky2night@users.noreply.github.com>
2022-05-16 20:37:51 +0800

    Add files via upload

---
+++ js.js
@@ -0,0 +1,14 @@
+function changebig()
+{
+    let get=document.getElementById('pic');
+    get.style.width='500px';
+    get.style.height='700px';
+}
+
+
+function changesamll()
+{
+    let get=document.getElementById('pic');
+    get.style.width='300px';
+    get.style.height='400px';
+}
